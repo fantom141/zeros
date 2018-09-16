008099f6dde1b16356ff340bbdc556553e725992
Alexander Balan <fantom141141@gmail.com>
2018-09-17 00:02:30 +0300

    Implements zeroCounter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    function fact(zeroCount, val, fac) {
+        if (Math.round(val / fac) >= 1) {
+            zeroCount += ((val - (val % fac)) / fac);
+            return fact(zeroCount, val, fac * 5);
+        }
+        return zeroCount;
+    }
+
+    return fact(0, number, 5);
 }
